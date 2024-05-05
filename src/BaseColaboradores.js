f5760429980babb23f72a43cd14e282503a7da6b
Fernanda <fernanda.rmonroy@gmail.com>
2024-05-05 10:42:28 -0400

    Base de dato colaboradores prototipo + dos colabs base extra

---
+++ src/BaseColaboradores.js
@@ -0,0 +1,42 @@
+export const BaseColaboradores = [
+  {
+    id: "1",
+    nombre: "Juan Soto",
+    correo: "juans@colaborador.com",
+    edad: "23",
+    cargo: "Desarrollador FrontEnd",
+    telefono: "99887766",
+  },
+  {
+    id: "2",
+    nombre: "Lucas Pailamilla",
+    correo: "lucasp@colaborador.com",
+    edad: "31",
+    cargo: "Desarrollador Backend",
+    telefono: "88779955",
+  },
+  {
+    id: "3",
+    nombre: "Diego Riquelme",
+    correo: "diegor@colaborador.com",
+    edad: "28",
+    cargo: "Ingeniero DevOps",
+    telefono: "99226644",
+  },
+  {
+    id: "4",
+    nombre: "Fernanda Rojas",
+    correo: "fernanda.rmonroy@gmail.com",
+    edad: "32",
+    cargo: "CEO",
+    telefono: "959044961",
+  },
+  {
+    id: "5",
+    nombre: "Fernando González",
+    correo: "fernando.gonzalez.031897@gmail.com",
+    edad: "28",
+    cargo: "Desarrollador Fullstack",
+    telefono: "9992188840",
+  },
+];
